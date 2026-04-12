c8429ea3a2b963e64d4531c162d6c5886fcb2dff
Cyrus Pyre <cyruspyre@gmail.com>
2026-04-12 21:52:00 +0530

    update fuzz

--- fuzz/fuzz_targets/parse_unchecked.rs
+++ fuzz/fuzz_targets/parse_unchecked.rs
@@ -8,7 +8,7 @@ use flexon_fuzz::AValue;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: AValue| unsafe {
-    let s0 = flexon::to_string(&data).unwrap_unchecked();
+    let s0 = flexon::to_string(data).unwrap_unchecked();
     let s1 = NullPadded::from_str(&s0);
     let s2 = Reader::new_unchecked(s0.as_bytes());
 
@@ -16,7 +16,7 @@ fuzz_target!(|data: AValue| unsafe {
     let r1 = Parser::new(&s1).parse_unchecked::<flexon::Value>();
     let r2 = Parser::new(s2).parse_unchecked::<flexon::OwnedValue>();
 
-    assert!(flexon::to_string(r0).unwrap_unchecked() == s0);
-    assert!(flexon::to_string(r1).unwrap_unchecked() == s0);
-    assert!(flexon::to_string(r2).unwrap_unchecked() == s0);
+    assert_eq!(flexon::to_string(&r0).unwrap_unchecked(), s0);
+    assert_eq!(r0, r1);
+    assert_eq!(r1, r2);
 });
